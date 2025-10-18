bd452d0d0c7ef1246b1956bfe74a7f5e2d5aca61
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-10-18 04:39:27 +0200

    implement my math helper :3

--- src/main/kotlin/org/ender_development/tinkeringworkshop/tiles/TileTinkeringWorkshop.kt
+++ src/main/kotlin/org/ender_development/tinkeringworkshop/tiles/TileTinkeringWorkshop.kt
@@ -1,16 +1,19 @@
 package org.ender_development.tinkeringworkshop.tiles
 
+import net.minecraft.init.Blocks
 import net.minecraft.util.ITickable
-import net.minecraft.util.math.BlockPos
 import org.ender_development.catalyx.client.AreaHighlighter
 import org.ender_development.catalyx.tiles.BaseTile
 import org.ender_development.catalyx.tiles.helper.IGuiTile
+import org.ender_development.catalyx.utils.math.BlockPositions
+import org.ender_development.catalyx.utils.math.Vec3
 import org.ender_development.tinkeringworkshop.TinkeringWorkshop
 import org.ender_development.tinkeringworkshop.config.ConfigHandler
 
 class TileTinkeringWorkshop :
     BaseTile(TinkeringWorkshop),
-    IGuiTile, ITickable {
+    IGuiTile,
+    ITickable {
 
     val ahNZ = AreaHighlighter()
     val ahPZ = AreaHighlighter()
@@ -18,29 +21,30 @@ class TileTinkeringWorkshop :
     val ahPX = AreaHighlighter()
 
     fun getEnchantingPower() {
-        val diameter = ConfigHandler.maxDiameter
-        val height = ConfigHandler.maxHeight
-        val radius = (diameter - 1) shr 1
-        val x = pos.x
-        val y = pos.y
-        val z = pos.z
-        val topY = y + height - 1
+        val height = ConfigHandler.maxHeight - 1 // exclude the block the tile is in
+        val radius = (ConfigHandler.maxDiameter - 1) shr 1
 
-        /*
+        val cuboid = BlockPositions.hollowCuboid(Vec3(pos.x, pos.y, pos.z), radius, height, 0)
+        val (wallPX, wallNX, wallPZ, wallNZ) = cuboid.map { BlockPositions.getAllInBox(it) }
 
-         */
+        // TODO ender: I have no idea if this highlighting works :/
+//        ahNZ.highlightArea(cuboid[0].first.toBlockPos(), cuboid[0].second.toBlockPos(), 0f, 1f, 0f, 2000)
+//        ahPZ.highlightArea(cuboid[1].first.toBlockPos(), cuboid[1].second.toBlockPos(), 0f, 1f, 0f, 2000)
+//        ahNX.highlightArea(cuboid[2].first.toBlockPos(), cuboid[2].second.toBlockPos(), 0f, 1f, 0f, 2000)
+//        ahPX.highlightArea(cuboid[3].first.toBlockPos(), cuboid[3].second.toBlockPos(), 0f, 1f, 0f, 2000)
 
-        // todo roz: fix this mess because my brain is asleep
-        val wallPX = BlockPos.getAllInBox(x + radius, y, z - radius + 1, x + radius - 1, topY, z + radius)
-        val wallNX = BlockPos.getAllInBox(x - radius, y, z - radius, x - radius + 1, topY, z + radius - 1)
-        val wallPZ = BlockPos.getAllInBox(x - radius, y, z + radius, x + radius - 1, topY, z + radius - 1)
-        val wallNZ = BlockPos.getAllInBox(x - radius + 1, y, z - radius + 1, x + radius, topY, z - radius)
+        wallPX.forEach { if (world.getBlockState(it).block == Blocks.AIR) world.setBlockState(it, Blocks.GOLD_BLOCK.defaultState) }
+        wallNX.forEach { if (world.getBlockState(it).block == Blocks.AIR) world.setBlockState(it, Blocks.DIAMOND_BLOCK.defaultState) }
+        wallPZ.forEach { if (world.getBlockState(it).block == Blocks.AIR) world.setBlockState(it, Blocks.EMERALD_BLOCK.defaultState) }
+        wallNZ.forEach { if (world.getBlockState(it).block == Blocks.AIR) world.setBlockState(it, Blocks.IRON_BLOCK.defaultState) }
     }
 
     var timer = 20
 
     override fun update() {
-        if(timer-- == 0)
+        if (timer-- == 0) {
             getEnchantingPower()
+            timer = 20
+        }
     }
 }
